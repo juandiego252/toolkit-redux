200f203dfef54b32ecbe1a5f23e0f488d18b3513
juandiego252 <juandiegojd252@gmail.com>
2024-11-07 13:35:42 -0500

    Redux toolkit

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
 
     const dispatch = useDispatch();
+    const { isLoading, pokemons, page } = useSelector(state => state.pokemons);
+
+
+
+
     useEffect(() => {
         dispatch(getPokemons());
     }, [])
@@ -13,11 +18,20 @@ export const PokemonApp = () => {
         <>
             <h1>Pokemon Api</h1>
             <hr />
+            <span>Loading: {isLoading ? 'True' : 'False'}</span>
             <ul>
-                <li>Pokemon 1</li>
-                <li>Pokemon 2</li>
-                <li>Pokemon 3</li>
+                {
+                    pokemons.map(({ name }) => (
+                        <li key={name}>{name}</li>
+                    ))
+                }
             </ul>
+            <button
+                disabled={isLoading}
+                onClick={() => dispatch(getPokemons(page))}
+            >
+                Next
+            </button>
         </>
     )
 }
